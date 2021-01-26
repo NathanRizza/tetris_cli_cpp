ff785a2800ef92c86a0403d6d5c985837a7ba9eb
NathanRizza <56983564+NathanRizza@users.noreply.github.com>
2021-01-25 21:45:21 -0500

    Created a function that can determine if the piece should move down

--- main.cpp
+++ main.cpp
@@ -7,13 +7,13 @@ using namespace std;
 
 const int boardx =10, boardy=15;
 int curPiece =0;
-bool piecePos[4][boardx][boardy];
+bool piecePos[boardx][boardy];
 bool lost =false;
 
 bool board[boardy][boardx] = {
 					{0,0,0,0,0,0,0,0,0,0},
 					{0,0,0,0,0,0,0,0,0,0},
-					{0,0,0,0,0,0,0,0,0,0},
+					{0,0,0,0,1,0,0,0,0,0},
 					{0,0,0,0,0,0,0,0,0,0},
 					{0,0,0,0,0,0,0,0,0,0},
 					{0,0,0,0,0,0,0,0,0,0},
@@ -31,21 +31,25 @@ bool board[boardy][boardx] = {
 //function declerations
 void gameOver();
 bool checkOverLap(int y,int x);
-void addAndCheck(int piece,int y,int x);
+void addAndCheck(int y,int x);
 void spawnPiece();
 void printBoard();
 void frameRule();
 void frame();
+bool canMoveDown();
 //---------------------
 
 
 int main(void)
 {
 	srand (time(NULL));
-	while(!lost)
-	{
-		frameRule();
-	}
+	spawnPiece();
+	frame();
+	canMoveDown();
+	//while(!lost)
+	//{
+	//	frameRule();
+	//}
 	cout<<"GAME OVER"<<endl;
 }
 
@@ -77,64 +81,93 @@ void spawnPiece(){
 	
 	if(p==0)//long
 	{
-		addAndCheck(0,0,3);
-		addAndCheck(1,0,4);
-		addAndCheck(2,0,5);
-		addAndCheck(3,0,6);
+		addAndCheck(0,3);
+		addAndCheck(0,4);
+		addAndCheck(0,5);
+		addAndCheck(0,6);
 		curPiece =0;
 	}
 	else if(p==1)//Backwards L
 	{
-		addAndCheck(0,0,3);
-		addAndCheck(1,1,3);
-		addAndCheck(2,1,4);
-		addAndCheck(3,1,5);
+		addAndCheck(0,3);
+		addAndCheck(1,3);
+		addAndCheck(1,4);
+		addAndCheck(1,5);
 		curPiece =1;
 	}
 	else if(p==2)//L
 	{
-		addAndCheck(0,0,5);
-		addAndCheck(1,1,3);
-		addAndCheck(2,1,4);
-		addAndCheck(3,1,5);
+		addAndCheck(0,5);
+		addAndCheck(1,3);
+		addAndCheck(1,4);
+		addAndCheck(1,5);
 		curPiece =2;
 	}
 	else if(p==3)//box
 	{
-		addAndCheck(0,0,4);
-		addAndCheck(1,0,5);
-		addAndCheck(2,1,4);
-		addAndCheck(3,1,5);
+		addAndCheck(0,4);
+		addAndCheck(0,5);
+		addAndCheck(1,4);
+		addAndCheck(1,5);
 		curPiece =3;
 	}
 	else if(p==4)//Backwards Z
 	{
-		addAndCheck(0,0,4);
-		addAndCheck(1,0,5);
-		addAndCheck(2,1,3);
-		addAndCheck(3,1,4);
+		addAndCheck(0,4);
+		addAndCheck(0,5);
+		addAndCheck(1,3);
+		addAndCheck(1,4);
 		curPiece =4;
 	}
 	else if(p==5)//Z
 	{
-		addAndCheck(0,0,3);
-		addAndCheck(1,0,4);
-		addAndCheck(2,1,4);
-		addAndCheck(3,1,5);
+		addAndCheck(0,3);
+		addAndCheck(0,4);
+		addAndCheck(1,4);
+		addAndCheck(1,5);
 		curPiece =5;
 	}
 	else//nub
 	{
-		addAndCheck(0,0,4);
-		addAndCheck(1,1,3);
-		addAndCheck(2,1,4);
-		addAndCheck(3,1,5);
+		addAndCheck(0,4);
+		addAndCheck(1,3);
+		addAndCheck(1,4);
+		addAndCheck(1,5);
 		curPiece =6;
 	}	
 	return;
 }
 
-void addAndCheck(int piece,int y,int x){
+//I am very proud of this function
+bool canMoveDown(){
+	int lowestInX[boardx]={0,0,0,0,0,0,0,0,0,0};
+
+		for(int i=0;i<boardy;i++)
+		{
+			
+			for(int j=0;j<boardx;j++)
+			{
+				if(piecePos[i][j])
+				{
+					lowestInX[j]=i;
+				}
+			}
+			
+		}
+	
+	for(int i=0;i<boardx;i++)
+	{
+		if(board[(lowestInX[i])-1][i])
+		{
+			cout<<"cannot move down"<<endl;
+			return false;
+		}
+	}
+	cout<<"can move down"<<endl;
+	return true;
+}
+
+void addAndCheck(int y,int x){
 	if(checkOverLap(y,x))
 	{
 		gameOver();
@@ -142,7 +175,7 @@ void addAndCheck(int piece,int y,int x){
 	else
 	{
 		board[y][x] = true;
-		piecePos[piece][y][x] =true;
+		piecePos[y][x] =true;
 	}
 }
 
@@ -173,3 +206,6 @@ void frame()
 {
 	printBoard();
 }
+
+
+
